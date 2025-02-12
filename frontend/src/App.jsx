b0cda9543a3892d20b314d8b98224b13ddc6ea6a
rushiphalle <rushikeshgphalle@gmail.com>
2025-02-12 22:21:42 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "/components.Navbar"
+import Home from "/components.Home"
+import Footer from "/components.Footer"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>  
+          <Route path="/" element={<Home />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
